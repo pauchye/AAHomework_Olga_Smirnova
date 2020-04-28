d4d57496df8bd4f1e514cb0b3807b8a3384c2d94
pauchye <olga.a.smi@gmail.com>
2020-04-27 23:56:32 -0400

    mailer and helper

---
+++ w7d2/solution/app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+    default from: 'everybody@appacademy.io'
+    def welcome_email(user)
+        @user = user
+        # @url  = 'http://example.com/login'
+        mail(to: @user.username, subject: 'Welcome to 99 cats')
+    end
+end
